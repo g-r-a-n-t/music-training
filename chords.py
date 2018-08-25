755dc3610760d7791d7cad932ff1b341e70632f8
Grant Wuerker <gwuerker@googlemail.com>
2018-08-24 19:58:36 -0500

    utils reworked and some options added

--- chords.py
+++ chords.py
@@ -8,31 +8,50 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-c", "--chords", help='chords used in training. Example: "maj min dim"', default='maj')
+parser.add_option("-s", "--scale-name", help='scale to base chord selection off of. Example: "major"', default='major')
 parser.add_option("-r", "--scale-root", help='root to base scale off of. Example: "C"', default='C')
-parser.add_option("-s", "--scale-name", help='chords are restricted to this scale. Example: "major"', default='major')
+parser.add_option("-w", "--written-hint", help='prints the name of the chord you are tasked with playing', action='store_true', default=False)
+parser.add_option("-a", "--audible-hint", help='audibly plays the chord you are tasked with guessing', action='store_true', default=False)
+parser.add_option("-t", "--two-hands", help='checks if the chord was played in both hands', action='store_true', default=False)
 (options, args) = parser.parse_args()
 
 keyboard = Keyboard()
 chord_names = options.chords.split();
 scale = Scale(Note(options.scale_root), options.scale_name)
 
+def hint(chord):
+    if options.written_hint:
+        print(str(chord))
+    if options.audible_hint:
+        keyboard.play(utils.notes_as_numbers(chord.notes))
+
+def check(played_notes, expected_chord):
+    played_counts = utils.numbers_as_counts(played_notes)
+    expected_set = set(utils.notes_as_counts(expected_chord.notes))
+
+    if set(played_counts) != expected_set:
+        return False
+
+    if options.two_hands and not all(count == 2 for count in played_counts.values()):
+        return False
+
+    return True
+
+
 while True:
     # generate a chord
     chord = None
     while not utils.chord_in_scale(chord, scale):
-        root = choice(scale.notes)
+        chord_root = choice(scale)
         chord_name = choice(chord_names)
-        chord = Chord(root, chord_name)
+        chord = Chord(chord_root, chord_name)
 
-    print(str(chord))
     # wait till user plays chord
-    flattened_chord = utils.flatten_notes(chord.notes)
+    print('new chord (press pedal for hint)')
     while True:
         played_notes = keyboard.next_state()
         if played_notes is None:
-            print('wrong')
-            #keyboard.play(NoteContainer(chord))
+            hint(chord)
         else:
-            flattened_played_notes = utils.flatten_numbers(played_notes)
-            if flattened_chord == flattened_played_notes:
+            if check(played_notes, chord):
                 break
--- utils.py
+++ utils.py
@@ -7,11 +7,27 @@ def chord_in_scale(chord, scale):
     if chord is None:
         return False
 
-    return flatten_notes(chord.notes).issubset(flatten_notes(scale.notes))
+    chord_set = set(notes_as_counts(chord.notes))
+    scale_set = set(notes_as_counts(scale.notes))
+    return chord_set.issubset(scale_set)
 
-def flatten_notes(notes):
-    return set(map(lambda note: note.number % 12, notes))
+def notes_as_numbers(notes):
+    numbers = []
+    for note in notes:
+        numbers.append(note.midi_note())
 
+    return numbers
 
-def flatten_numbers(numbers):
-    return set(map(lambda number: number % 12, numbers))
+def numbers_as_counts(numbers):
+    counts = {}
+    for number in numbers:
+        octave_number = number % 12
+        if octave_number in counts:
+            counts[octave_number] += 1
+        else:
+            counts[octave_number] = 1
+
+    return counts
+
+def notes_as_counts(notes):
+    return numbers_as_counts(notes_as_numbers(notes))
